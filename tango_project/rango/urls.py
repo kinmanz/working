68f3c0d95e158c6c235e3bcf8a53b71ed7be5bc8
kinmanz <luga2012@yandex.ru>
2016-03-14 14:29:47 +0300

    First app

---
+++ tango_project/rango/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, url
+from django.conf import settings
+from rango import views
+
+urlpatterns = [
+        url(r'^$', views.index, name='index'),
+        url(r'^about/$', views.index_about, name='index_about'),
+        url(r'^new/$', views.index2, name='index2')]
+
+# urlpatterns = [
+#     url(r'^admin/', admin.site.urls),
+#     # Examples:
+#     # url(r'^$', 'tango_with_django_project_17.views.home', name='home'),
+#     # url(r'^blog/', include('blog.urls')),
+#     url(r'^rango/', include('rango.urls')),  # ADD THIS NEW TUPLE!
+# ]
+
+
