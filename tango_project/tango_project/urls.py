ea62f5d4bb26ed9d7eb8b7c3ea4fae7a99a4b4fd
kinmanz <luga2012@yandex.ru>
2016-03-18 15:04:29 +0300

    Some modifications with urls, templates by new allowed functions in Django 1.9

--- tango_project/tango_project/urls.py
+++ tango_project/tango_project/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls import include, patterns, url
 from django.conf import settings
 from django.contrib import admin
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # Examples:
